d8d6c66a6bba5e78df0d7dc2836f5a183f0061a0
mern-hash <mernhash@gmail.com>
2024-09-03 17:57:52 +0530

    UI

--- src/page-components/projects/CongratulationsModal.js
+++ src/page-components/projects/CongratulationsModal.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { Modal, Input, Divider, Checkbox, Progress, Button, Steps } from 'antd'
 import { PrimaryButton } from 'elements' // Assume this is your custom button component
 import styled from 'styled-components'
@@ -8,9 +8,6 @@ import HomeImg from '../../images/ModalHome.png'
 const CongratulationsBodyWrapper = styled.div`
   height: calc(100vh - 165px);
   overflow: auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   &::-webkit-scrollbar {
     width: 5px;
   }
@@ -23,7 +20,22 @@ const CongratulationsBodyWrapper = styled.div`
     border-radius: 50px;
   }
   .CongratulationsBox {
-    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100%;
+    animation: 1s alternate ShowOut;
+    animation-delay: 2s;
+    @keyframes ShowOut {
+      from {
+        opacity: 1;
+      }
+
+      to {
+        opacity: 0;
+      }
+    }
     img {
       width: 110px;
       height: auto;
@@ -33,6 +45,66 @@ const CongratulationsBodyWrapper = styled.div`
     h5 {
       text-align: center;
       margin-top: 16px;
+    }
+  }
+  .OrderStatusBox {
+    padding-top: 40px;
+    // transition: transform 0.5s ease, opacity 0.5s ease;
+    animation: 3s alternate ShowIn;
+    @keyframes ShowIn {
+      from {
+        opacity: 0;
+      }
+
+      to {
+        opacity: 1;
+      }
+    }
+
+    h5.MainTitle {
+      font-size: 16px;
+      margin-bottom: 6px;
+    }
+    p.MainSubTitle {
+      color: #848484;
+      font-size: 13px;
+      margin-bottom: 40px;
+    }
+    .ant-steps {
+      .ant-steps-item {
+        &.ant-steps-item-process {
+          .ant-steps-item-icon {
+            background-color: #312438;
+            border-color: #312438;
+          }
+        }
+        &.ant-steps-item-finish {
+          .ant-steps-item-icon {
+            background-color: #3124380a;
+            .ant-steps-icon {
+              color: #312438;
+            }
+          }
+          .ant-steps-item-tail::after {
+            background-color: #312438;
+          }
+        }
+        .ant-steps-item-content {
+          .ant-steps-item-title {
+            font-size: 12px;
+            color: #000000;
+            font-weight: 600;
+          }
+          .ant-steps-item-description {
+            font-size: 12px;
+            padding-bottom: 40px;
+            h6 {
+              font-size: 12px;
+              margin-bottom: 2px;
+            }
+          }
+        }
+      }
     }
   }
 `
@@ -53,6 +125,7 @@ const CongratulationsModal = ({ openDrawer, setOpenDrawer }) => {
   const [reviewing, setReviewing] = useState(false)
   const [securing, setSecuring] = useState(false)
   const [initials, setInitials] = useState('')
+  const [show, setShow] = useState(true)
 
   const handleReviewClick = () => {
     if (reviewing) setSecuring(true)
@@ -65,6 +138,13 @@ const CongratulationsModal = ({ openDrawer, setOpenDrawer }) => {
     setOpenDrawer(false)
   }
 
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      console.log('show toggle')
+      setShow(false)
+    }, 3500)
+  }, [])
+
   return (
     <Modal
       open={openDrawer}
@@ -74,12 +154,58 @@ const CongratulationsModal = ({ openDrawer, setOpenDrawer }) => {
       closeIcon={false}
     >
       <CongratulationsBodyWrapper>
-        <div className="CongratulationsBox">
-          <img src={HomeImg} alt="" />
-          <h5>
-            Congratulations! Your investment in The Sedgefield is complete.
-          </h5>
-        </div>
+        {show ? (
+          <div className="CongratulationsBox">
+            <img src={HomeImg} alt="" />
+            <h5>
+              Congratulations! Your investment in The Sedgefield is complete.
+            </h5>
+          </div>
+        ) : (
+          <div className="OrderStatusBox">
+            <h5 className="MainTitle">
+              You're on your way to your first expected dividends!
+            </h5>
+            <p className="MainSubTitle">
+              Here's a look at what's next in the meantime;
+            </p>
+            <Steps
+              direction="vertical"
+              size="small"
+              current={1}
+              items={[
+                {
+                  title: 'TODAY',
+                  description: (
+                    <>
+                      <h6>Order Confirmed</h6>
+                      10 shares @ $10 each
+                    </>
+                  ),
+                },
+                {
+                  title: 'BY NEXT WEEK',
+                  description: (
+                    <>
+                      <h6>Trade processed</h6>
+                      You can expect the money to move from your bank account in
+                      the next 3-5 business days
+                    </>
+                  ),
+                },
+                {
+                  title: 'ONCE PROPERTY IS RENTED',
+                  description: (
+                    <>
+                      <h6>Monthly dividends paid</h6>Dividends are paid monthly.
+                      beginning shortly after the property is rented.
+                    </>
+                  ),
+                },
+              ]}
+            />
+          </div>
+        )}
       </CongratulationsBodyWrapper>
 
       <BottomButtonWrapper>
