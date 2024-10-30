f3a2af7657e0619028d67dd9bb527509caf260eb
Siddharth-DWT <smishra9120@gmail.com>
2024-10-30 18:06:01 +0530

    show only available offers

--- src/hooks/projects.js
+++ src/hooks/projects.js
@@ -26,7 +26,7 @@ const initialSearch = {
   assetType: [],
   status: [],
   addressLocation: 'all',
-  projectMixStatus: [],
+  projectMixStatus: ['available'],
 }
 
 export const GetProjectsHook = (ownerOnly, tab = ProjectFilters.all) => {
@@ -282,7 +282,7 @@ export const GetApprovedProjectsHook = (currentTab) => {
     totalPerPage: 12,
     pageNumber: 1,
     searchFilter: {
-      projectMixStatus: [],
+      projectMixStatus: ['available'],
     },
   })
 
